21f250ee9dfa9e58fd024ee0fa818aecba28a80b
Elin Simenstad <elin_sim@hotmail.com>
2025-09-03 13:04:28 +0200

    updated previous workshops based on feedback

--- WebDev/2-Javascript/week 3 workshop/registration.js
+++ WebDev/2-Javascript/week 3 workshop/registration.js
@@ -1,90 +1,125 @@
 class Student {
-  constructor(name, email) {
-    this.name = name;
-    this.email = email;
-  }
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+    }
 }
 
 class Bootcamp {
-  constructor(name, level) {
-    this.name = name;
-    this.level = level;
-    this.students = [];
-  }
-
-  registerStudent(studentToRegister) {
-    if (studentToRegister.name && studentToRegister.email) {
-      const studentExists = this.students.find(
-        (s) =>
-          s.name === studentToRegister.name ||
-          s.email === studentToRegister.email
-      );
-
-      if (studentExists) {
-        console.log("Student already registered:", studentToRegister.name);
-        return false;
-      } else {
-        console.log("Added student:", studentToRegister.name);
-        this.students.push(studentToRegister);
-        return true;
-      }
-    } else {
-      console.log("Invalid name or email");
-      return false;
+    constructor(name, level) {
+        this.name = name;
+        this.level = level;
+        this.students = [];
     }
-  }
 
-  listStudents() {
-    if (this.students.length === 0) {
-      console.log(`No students are registered to the ${this.name} bootcamp.`);
-      return false;
-    } else {
-      console.log(
-        `The students registered in ${this.name} are: \n${this.students
-          .map((s) => `${s.name} / ${s.email}`)
-          .join("\n")}`
-      );
-      return true;
+    registerStudent(studentToRegister) {
+        if (studentToRegister.name && studentToRegister.email) {
+            const studentExists = this.students.find(
+                (s) =>
+                    s.name === studentToRegister.name &&
+                    s.email === studentToRegister.email
+            );
+
+            if (studentExists) {
+                console.log(
+                    "Student already registered:",
+                    studentToRegister.name
+                );
+                return false;
+            } else {
+                console.log("Added student:", studentToRegister.name);
+                this.students.push(studentToRegister);
+                return true;
+            }
+        } else {
+            console.log("Invalid name or email");
+            return false;
+        }
     }
-  }
+
+    removeStudent(email) {
+        const studentExists = this.students.find((s) => s.email === email);
+        if (studentExists) {
+            var filteredArray = this.students.filter((s) => s.email !== email);
+            this.students = filteredArray;
+            console.log("Removed student with email:", email);
+
+            return true;
+        } else {
+            console.log("No student exists with email:", email);
+            return false;
+        }
+    }
+
+    listStudents() {
+        if (this.students.length === 0) {
+            console.log(
+                `No students are registered to the ${this.name} bootcamp.`
+            );
+            return false;
+        } else {
+            /*console.log(
+                `The students registered in ${this.name} are: \n${this.students
+                    .map((s) => `${s.name} / ${s.email}`)
+                    .join("\n")}`
+            );*/
+            console.log(`The students registered in ${this.name} are:`);
+            this.students.forEach((s) =>
+                console.log(`Name: ${s.name}   Email: ${s.email}`)
+            );
+            return true;
+        }
+    }
+
+    getInfo() {
+        console.log(`This is the Bootcamp for ${this.level} ${this.name} `);
+    }
 }
 
 //Test
 testStudent = new Student("Bugs Bunny", "bugs@bunny.com");
 console.log(testStudent);
 if (
-  testStudent.name === "Bugs Bunny" &&
-  testStudent.email === "bugs@bunny.com"
+    testStudent.name === "Bugs Bunny" &&
+    testStudent.email === "bugs@bunny.com"
 ) {
-  console.log("TASK 1: PASS");
+    console.log("TASK 1: PASS");
 }
 
 reactBootcamp = new Bootcamp("React", "Advanced");
 console.log(reactBootcamp);
 if (
-  reactBootcamp.name === "React" &&
-  reactBootcamp.level === "Advanced" &&
-  Array.isArray(reactBootcamp.students) &&
-  reactBootcamp.students.length === 0
+    reactBootcamp.name === "React" &&
+    reactBootcamp.level === "Advanced" &&
+    Array.isArray(reactBootcamp.students) &&
+    reactBootcamp.students.length === 0
 ) {
-  console.log("TASK 2: PASS");
+    console.log("TASK 2: PASS");
 }
 
 const runTest = (bootcamp, student) => {
-  const attemptOne = bootcamp.registerStudent(student);
-  const attemptTwo = bootcamp.registerStudent(student);
-  const attemptThree = bootcamp.registerStudent(new Student("Babs Bunny"));
-  if (attemptOne && !attemptTwo && !attemptThree) {
-    console.log("TASK 3: PASS");
-  }
+    const attemptOne = bootcamp.registerStudent(student);
+    const attemptTwo = bootcamp.registerStudent(student);
+    const attemptThree = bootcamp.registerStudent(new Student("Babs Bunny"));
+    if (attemptOne && !attemptTwo && !attemptThree) {
+        console.log("TASK 3: PASS");
+    }
 
-  bootcamp.registerStudent(new Student("Babs Bunny", "babs@bunny.com"));
-  if (bootcamp.listStudents()) {
-    console.log("TASK 4: PASS 1/2");
-  }
-  bootcamp.students = [];
-  if (!bootcamp.listStudents()) {
-    console.log("TASK 4: PASS 2/2");
-  }
+    bootcamp.registerStudent(new Student("Babs Bunny", "babs@bunny.com"));
+    if (bootcamp.listStudents()) {
+        console.log("TASK 4: PASS 1/2");
+    }
+    bootcamp.students = [];
+    if (!bootcamp.listStudents()) {
+        console.log("TASK 4: PASS 2/2");
+    }
+
+    bootcamp.registerStudent(new Student("Bugs Bunny", "bugs@bunny.com"));
+    bootcamp.registerStudent(new Student("Babs Bunny", "babs@bunny.com"));
+    bootcamp.removeStudent("babs22@bunny.com");
+    bootcamp.removeStudent("babs@bunny.com");
+    bootcamp.listStudents();
 };
 runTest(reactBootcamp, testStudent);
+
+reactBootcamp.getInfo();
