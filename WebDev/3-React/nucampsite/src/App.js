679f6807c55e0544e63f1de9c634e4ad822d58f4
Elin Simenstad <elin_sim@hotmail.com>
2025-09-11 09:03:04 +0200

    Async Promotions and Displaylist

--- WebDev/3-React/nucampsite/src/App.js
+++ WebDev/3-React/nucampsite/src/App.js
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { fetchCampsites } from "./features/campsites/campsitesSlice.js";
 import { fetchPartners } from "./features/partners/partnersSlice.js";
+import { fetchPromotions } from "./features/promotions/promotionsSlice.js";
 
 import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
@@ -19,6 +20,7 @@ function App() {
     useEffect(() => {
         dispatch(fetchCampsites());
         dispatch(fetchPartners());
+        dispatch(fetchPromotions());
     }, [dispatch]);
 
     return (
