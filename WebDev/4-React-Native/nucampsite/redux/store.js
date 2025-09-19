706e58c7220dd6ca546d6cfc76eb641bf32e372e
Elin <elin_sim@hotmail.com>
2025-09-19 14:16:49 +0200

    Persist Redux Store

--- WebDev/4-React-Native/nucampsite/redux/store.js
+++ WebDev/4-React-Native/nucampsite/redux/store.js
@@ -5,12 +5,35 @@ import { partnersReducer } from "../features/partners/partnersSlice";
 import { promotionsReducer } from "../features/promotions/promotionsSlice";
 import { favoritesReducer } from "../features/favorites/favoritesSlice";
 
+import {
+    persistStore,
+    persistCombineReducers,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from "redux-persist";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getDefaultTarget } from "expo/config";
+
+const config = { key: "root", storage: AsyncStorage, debug: true };
+
 export const store = configureStore({
-    reducer: {
+    reducer: persistCombineReducers(config, {
         campsites: campsitesReducer,
         comments: commentsReducer,
         partners: partnersReducer,
         promotions: promotionsReducer,
         favorites: favoritesReducer,
-    },
+    }),
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 });
+
+export const persistor = persistStore(store);
