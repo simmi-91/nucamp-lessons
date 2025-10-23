3f38d345edf160e44de9160ab715286516f4316b
Elin Simenstad <elin_sim@hotmail.com>
2025-10-23 13:06:32 +0200

    Express Session Part 2

--- WebDev/5-NodeJS-Express-MongoDB/nucampsiteServer/app.js
+++ WebDev/5-NodeJS-Express-MongoDB/nucampsiteServer/app.js
@@ -1,7 +1,7 @@
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
+//var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const session = require("express-session");
 const FileStore = require("session-file-store")(session);
@@ -42,33 +42,18 @@ app.use(
     })
 );
 
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
 function auth(req, res, next) {
     console.log(req.session);
 
     if (!req.session.user) {
-        const authHeader = req.headers.authorization;
-        if (!authHeader) {
-            const err = new Error("You are not authenticated!");
-            res.setHeader("WWW-Authenticate", "Basic");
-            err.status = 401;
-            return next(err);
-        }
-
-        const auth = Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":");
-        const user = auth[0];
-        const pass = auth[1];
-        if (user === "admin" && pass === "password") {
-            //res.cookie("user", "admin", { signed: true });
-            req.session.user = "admin";
-            return next(); // authorized
-        } else {
-            const err = new Error("You are not authenticated!");
-            res.setHeader("WWW-Authenticate", "Basic");
-            err.status = 401;
-            return next(err);
-        }
+        const err = new Error("You are not authenticated!");
+        err.status = 401;
+        return next(err);
     } else {
-        if (req.session.user === "admin") {
+        if (req.session.user === "authenticated") {
             return next();
         } else {
             const err = new Error("You are not authenticated!");
@@ -81,8 +66,6 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/campsites", campsiteRouter);
 app.use("/promotions", promotionRouter);
 app.use("/partners", partnerRouter);
